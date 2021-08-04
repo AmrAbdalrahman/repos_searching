f74a326a690a152884aa76d614d32c51a91ceb5f
AmrAbdalrahman <amr.abdalrahman94@gmail.com>
2021-08-04 19:09:49 +0200

    test cases

--- tests/Feature/RepoFilteringTest.php
+++ tests/Feature/RepoFilteringTest.php
@@ -19,4 +19,268 @@ class RepoFilteringTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     * /api/v1/repos?sort=stars&order=desc
+     *
+     * @return void
+     */
+    public function testShouldReturnAllReposSortByStarsAndOrderByDesc()
+    {
+
+        $response = $this->get("api/v1/repos?sort=stars&order=desc");
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => true,
+            "error" => null,
+        ]);
+        $response->assertJsonStructure(
+            ['data' =>
+
+                [
+                    'total_count',
+                    'incomplete_results',
+                    'items' =>
+                        [
+                            '*' => [
+                                'id',
+                                'node_id',
+                                'name',
+                                'full_name',
+                                'private',
+                                'owner',
+                                'html_url',
+                                'url',
+                                'forks_url',
+                                'teams_url',
+                                'language',
+                                'has_wiki',
+                            ]
+                        ],
+                ],
+                'status',
+                'error'
+            ]
+        );
+    }
+
+    /**
+     * /api/v1/repos?sort=stars&order=desc&top_views=100
+     *
+     * @return void
+     */
+    public function testShouldReturnAllReposSortByStarsAndOrderByDescAndTopViews()
+    {
+
+        $response = $this->get("api/v1/repos?sort=stars&order=desc&top_views=100");
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => true,
+            "error" => null,
+        ]);
+        $response->assertJsonStructure(
+            ['data' =>
+
+                [
+                    'total_count',
+                    'incomplete_results',
+                    'items' =>
+                        [
+                            '*' => [
+                                'id',
+                                'node_id',
+                                'name',
+                                'full_name',
+                                'private',
+                                'owner',
+                                'html_url',
+                                'url',
+                                'forks_url',
+                                'teams_url',
+                                'language',
+                                'has_wiki',
+                            ]
+                        ],
+                ],
+                'status',
+                'error'
+            ]
+        );
+    }
+
+    /**
+     * /api/v1/repos?sort=stars&order=desc&language=php
+     *
+     * @return void
+     */
+    public function testShouldReturnAllReposSortByStarsAndOrderByDescAndLanguage()
+    {
+
+        $response = $this->get("api/v1/repos?sort=stars&order=desc&language=php");
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => true,
+            "error" => null,
+        ]);
+        $response->assertJsonStructure(
+            ['data' =>
+
+                [
+                    'total_count',
+                    'incomplete_results',
+                    'items' =>
+                        [
+                            '*' => [
+                                'id',
+                                'node_id',
+                                'name',
+                                'full_name',
+                                'private',
+                                'owner',
+                                'html_url',
+                                'url',
+                                'forks_url',
+                                'teams_url',
+                                'language',
+                                'has_wiki',
+                            ]
+                        ],
+                ],
+                'status',
+                'error'
+            ]
+        );
+    }
+
+    /**
+     * /api/v1/repos?sort=stars&order=desc&start_date=2018-09-30
+     *
+     * @return void
+     */
+    public function testShouldReturnAllReposSortByStarsAndOrderByDescAndStartDate()
+    {
+
+        $response = $this->get("api/v1/repos?sort=stars&order=desc&start_date=2018-09-30");
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => true,
+            "error" => null,
+        ]);
+        $response->assertJsonStructure(
+            ['data' =>
+
+                [
+                    'total_count',
+                    'incomplete_results',
+                    'items' =>
+                        [
+                            '*' => [
+                                'id',
+                                'node_id',
+                                'name',
+                                'full_name',
+                                'private',
+                                'owner',
+                                'html_url',
+                                'url',
+                                'forks_url',
+                                'teams_url',
+                                'language',
+                                'has_wiki',
+                            ]
+                        ],
+                ],
+                'status',
+                'error'
+            ]
+        );
+    }
+
+    /**
+     * /api/v1/repos?sort=stars&order=desc&language=php&start_date=2018-09-30
+     *
+     * @return void
+     */
+    public function testShouldReturnAllReposSortByStarsAndOrderByDescAndStartDateAndLanguage()
+    {
+
+        $response = $this->get("api/v1/repos?sort=stars&order=desc&language=php&start_date=2018-09-30");
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => true,
+            "error" => null,
+        ]);
+        $response->assertJsonStructure(
+            ['data' =>
+
+                [
+                    'total_count',
+                    'incomplete_results',
+                    'items' =>
+                        [
+                            '*' => [
+                                'id',
+                                'node_id',
+                                'name',
+                                'full_name',
+                                'private',
+                                'owner',
+                                'html_url',
+                                'url',
+                                'forks_url',
+                                'teams_url',
+                                'language',
+                                'has_wiki',
+                            ]
+                        ],
+                ],
+                'status',
+                'error'
+            ]
+        );
+    }
+
+    /**
+     * /api/v1/repos?sort=stars&order=desc&language=php&start_date=2018-09-30&top_views=100
+     *
+     * @return void
+     */
+    public function testShouldReturnAllReposWithAllFilters()
+    {
+
+        $response = $this->get("api/v1/repos?sort=stars&order=desc&language=php&start_date=2018-09-30&top_views=100");
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => true,
+            "error" => null,
+        ]);
+        $response->assertJsonStructure(
+            ['data' =>
+
+                [
+                    'total_count',
+                    'incomplete_results',
+                    'items' =>
+                        [
+                            '*' => [
+                                'id',
+                                'node_id',
+                                'name',
+                                'full_name',
+                                'private',
+                                'owner',
+                                'html_url',
+                                'url',
+                                'forks_url',
+                                'teams_url',
+                                'language',
+                                'has_wiki',
+                            ]
+                        ],
+                ],
+                'status',
+                'error'
+            ]
+        );
+    }
 }
